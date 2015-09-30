79c98ed60453b934b6a7ea28027bdc5e7444a52d
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2015-09-30 20:06:10 +0300

    improve

--- formutils.go
+++ formutils.go
@@ -2,23 +2,25 @@ package formutils
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"reflect"
 	"time"
 
+	"github.com/ilgooz/eres"
 	"github.com/ilgooz/schema"
 	"gopkg.in/go-playground/validator.v8"
 )
 
 var (
-	ParsingMessages = map[reflect.Type][]string{
+	ParsingErrorMessages = map[reflect.Type][]string{
 		reflect.TypeOf(time.Time{}): []string{"must be a UTC date", "must be group of UTC dates"},
 		reflect.TypeOf(0):           []string{"must be a number", "must be group of numbers"},
 		reflect.TypeOf(0.0):         []string{"must be a number", "must be group of numbers"},
 		reflect.TypeOf(false):       []string{"must be boolean", ""},
 	}
 
-	ValidationMessages = map[string][]string{
+	ValidationErrorMessages = map[string][]string{
 		"email": []string{"must be a valid email address", "must be group of valid email addresses"},
 		"min":   []string{"must be min %s chars length", "items must be min %s chars length"},
 	}
@@ -58,6 +60,18 @@ func Parse(r *http.Request, out interface{}) (invalids map[string]string, err er
 	return invalids, nil
 }
 
+func ParseSend(w http.ResponseWriter, r *http.Request, out interface{}) (invalid bool) {
+	invalids, err := Parse(r, out)
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return true
+	}
+
+	return eres.New(w).SetFields(invalids).WeakSend()
+}
+
 func parseForm(out interface{}, r *http.Request) (invalids map[string]string, err error) {
 	invalids = make(map[string]string)
 
@@ -77,21 +91,21 @@ func parseForm(out interface{}, r *http.Request) (invalids map[string]string, er
 
 	for _, multiErr := range multiErrs {
 		convErr, ok := multiErr.(schema.ConversionError)
+
 		if !ok {
 			return invalids, err
 		}
 
-		messages, exists := ParsingMessages[convErr.Type]
 		var message string
 
-		if exists {
+		if messages, exists := ParsingErrorMessages[convErr.Type]; exists {
 			if convErr.Index == -1 {
 				message = messages[0]
 			} else {
 				message = messages[1]
 			}
 		} else {
-			message = "unknown error"
+			message = "invalid"
 		}
 
 		invalids[convErr.Key] = message
@@ -104,6 +118,7 @@ func validateForm(out interface{}) (invalids map[string]string) {
 	invalids = make(map[string]string)
 
 	err := validate.Struct(out)
+
 	if err == nil {
 		return invalids
 	}
@@ -111,9 +126,7 @@ func validateForm(out interface{}) (invalids map[string]string) {
 	for _, e := range err.(validator.ValidationErrors) {
 		var message string
 
-		messages, exists := ValidationMessages[e.Tag]
-
-		if exists {
+		if messages, exists := ValidationErrorMessages[e.Tag]; exists {
 			if e.Kind == reflect.Slice {
 				message = messages[1]
 			} else {
@@ -124,7 +137,7 @@ func validateForm(out interface{}) (invalids map[string]string) {
 				message = fmt.Sprintf(message, e.Param)
 			}
 		} else {
-			message = e.Tag
+			message = fmt.Sprintf("%s: %s", e.Tag, e.Param)
 		}
 
 		invalids[e.Name] = message
